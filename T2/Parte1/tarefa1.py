4c45c785afa0928964f6c7dfe4cc3833776a3d00
fcp16 <fcp16@inf.ufpr.br>
2021-02-22 17:46:22 -0300

    pratica 2 concluida

--- T2/Parte1/tarefa1.py
+++ T2/Parte1/tarefa1.py
@@ -9,28 +9,59 @@ print('===================\n')
 print('Permissões por APK\n')
 print('===================')
 
+dic = {}
 
-for filename in os.listdir('manifestos'):
+for filename in os.listdir('manifestos'): #para cada xml existente no diretório
     if filename.endswith(".xml"):
-        arq = os.path.join(d,filename)
-        arqNome = arq.split('.')[0]
 
-        arq_saida1 = arqNome + '.json'  
-        saida1 = open(arq_saida1,'w')
+        arq = os.path.join(d,filename) #arquivo com extensão xml
+        arqNome = arq.split('.')[0] 
 
-        with open(arq) as fd:
+        str_json = arqNome + '.json'  
+        arq_json = open(str_json,'w') #abre o arquivo json para escrita
+
+        with open(arq) as fd: #converte o arquivo xml para json
             doc = xmltodict.parse(fd.read())
-        saida1.write(json.dumps(doc))
+        arq_json.write(json.dumps(doc))
 
         saida = []
 
-        with open(arq_saida1) as fd2:
+        with open(str_json) as fd2:
             jsonFile = json.load(fd2)
             
             userPerm =jsonFile['manifest']['uses-permission']
             for row in userPerm:
                 for r in row:
                     if 'name' in r :
-                        saida.append(row[r].split('.').pop())
+                        saida.append(row[r].split('.').pop()) #SALVA A LISTA DE PERMISSAO DE CADA APK
         print('\n'+arqNome+':')   
         print(str(saida))   
+
+        dic[arqNome] = set(saida)
+
+print('\n=========================\n')
+print('Permissões comuns das APKs\n')
+print('=========================\n')
+
+intersec = None 
+for k in dic :
+    if intersec :
+        intersec = intersec.intersection(dic[k])
+    else:
+        intersec = dic[k]
+print (intersec)
+
+print('\n==========================\n')
+print('Permissões únicas por APK\n')
+print('==========================\n')
+
+for k1 in dic:
+    result = dic[k1] 
+    for k2 in dic:
+        if k1 is not k2:
+            result = result.difference(dic[k2])
+    if len(result) > 0 :
+        print (k1,':',result)
+    else :
+        print (k1,': não possui nenhuma chave diferentona!')
+
